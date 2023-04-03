647d932d145b1773e611f5579d1f6697fa9164a8
collins <collinsmarwah@gmail.com>
2023-04-03 04:21:59 -0700

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,11 +13,11 @@ unsigned int _strspn(char *s, char *accept)
 
 	value = 0;
 
-	for (i = 0; s[i] != '\0'; n++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		check = 0;
 
-		for (n = 0; accept[i] != '\0'; n++)
+		for (n = 0; accept[n] != '\0'; n++)
 		{
 			if (accept[n] == s[i])
 			{
